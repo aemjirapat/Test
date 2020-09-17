050cd00d8dd904aeee8d4409b372c93022a588be
aem-jirapat <aemjirapat.tr@gmail.com>
2020-09-18 00:32:14 +0700

    Update

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -4,13 +4,12 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class BindingUtils{
-    @BindingAdapter("setDuration")
-    fun TextView.setDuration(item: SleepNight) {
-        text = "Hello"//item.startTimeMilli.toString() + ", " +item.endTimeMilli.toString() + " !!!"
-    }
-    @BindingAdapter("setSleepQuality")
-    fun TextView.setSleepQuality(item: SleepNight) {
-        text = " world"//item.sleepQuality.toString() + "!!!"
-    }
+@BindingAdapter("setDuration")
+fun TextView.setDuration(item: SleepNight) {
+    text = "Hello"//item.startTimeMilli.toString() + ", " +item.endTimeMilli.toString() + " !!!"
+}
+
+@BindingAdapter("setSleepQuality")
+fun TextView.setSleepQuality(item: SleepNight) {
+    text = " world"//item.sleepQuality.toString() + "!!!"
 }
